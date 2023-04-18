5510107ff999dfeac19edec47459c256250bcedc
moldybaguette <45487141+moldybaguette@users.noreply.github.com>
2023-04-18 18:43:51 +0200

    Add project files.

---
+++ Classes/Ingredient.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgPoe.Classes
+{
+    internal class Ingredient
+    {
+        private String IngredientName;
+        private float IngredientScale = 1;
+        private float IngredientAmount;
+        private string IngredientMesurementUnit = "tablespoon";
+
+        public Ingredient()
+        {
+
+        }
+
+        public Ingredient(string name, float scale, float amount, string mesurementUnit)
+        {
+            IngredientName = name;
+            IngredientScale = scale;
+            IngredientAmount = amount;
+            IngredientMesurementUnit = mesurementUnit;
+        }
+
+        public String Name(String name)
+        {
+            IngredientName = name;
+            return IngredientName;
+        }
+
+        public float Amount(float amount)
+        {
+            IngredientAmount = amount;
+            return IngredientAmount * IngredientScale;
+        }
+
+        public float SetScale(float scale)
+        {
+            IngredientScale = scale;
+            return IngredientScale;
+        }
+
+
+    }
+}
---
+++ Classes/Recipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgPoe.Classes
+{
+    internal class Recipe
+    {
+        private Ingredient[] Ingredients;
+
+
+
+        public Recipe(int NumberOfIngredients)
+        {
+            Ingredients = new Ingredient[NumberOfIngredients];
+        }
+    }
+}
